521ec0b0c7aa52940e091a95064fa7d2ee577517
keroles adel <kerolesadel297@gmail.com>
2026-07-12 11:34:32 +0200

    solve error deploy

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,18 +1,21 @@
 import { initializeApp, getApps, cert, App } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
- 
+
 let adminApp: App;
- 
+
 if (!getApps().length) {
+  const decoded = Buffer.from(
+    process.env.FIREBASE_SERVICE_ACCOUNT_BASE64!,
+    "base64"
+  ).toString("utf-8");
+
+  const serviceAccount = JSON.parse(decoded);
+
   adminApp = initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+    credential: cert(serviceAccount),
   });
 } else {
   adminApp = getApps()[0];
 }
- 
+
 export const adminDb = getFirestore(adminApp);
